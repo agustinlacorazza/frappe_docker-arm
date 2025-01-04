0970a8dfbdc6361ce0d53d4c09ee37f0e5dae06f
agustinlacorazza <agustinpablom@gmail.com>
2025-01-03 19:15:50 -0500

    .

---
+++ socketio/socketio.js
@@ -0,0 +1,8 @@
+const io = require('socket.io')(9000);
+
+io.on('connection', (socket) => {
+  console.log('Nuevo cliente conectado');
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado');
+  });
+});
